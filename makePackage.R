2dc623465fe1c307cc9c8144b49cb39ca6b13a27
Benjamin P. Kellman <bkellman@ucsd.edu>
2015-03-27 18:30:48 -0700

    code to easily make an R package

---
+++ makePackage.R
@@ -0,0 +1,7 @@
+## https://www.youtube.com/watch?v=9PyQlbAEujY
+
+library('devtools')
+library('roxygen2')
+
+RStudio -> Build -> Configure Build Tools 
+  -> Generate Documentation with Roxygen (select all)
